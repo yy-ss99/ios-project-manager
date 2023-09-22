780f0fe8d0ea804a43d1cd1d77930aaa9ca60de3
yyss99 <yyssjj9999@gmail.com>
2023-09-22 15:29:10 +0900

    feat: configureNavigationBar 메서드 추가 및 구현, tapAddButton 추가

--- ProjectManager/ProjectManager/ViewController.swift
+++ ProjectManager/ProjectManager/ViewController.swift
@@ -10,9 +10,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        configureNavigationBar()
     }
 
-
+    func configureNavigationBar() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add
+                                                            , target: self, action: #selector(tapAddButton))
+        navigationItem.title = "Project Manager"
+        
+        let navigationBarAppearance = UINavigationBarAppearance()
+        navigationBarAppearance.backgroundColor = .systemGray6
+        navigationController?.navigationBar.standardAppearance = navigationBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navigationBarAppearance
+    }
+    
+    @objc func tapAddButton() {
+        
+    }
 }
 
