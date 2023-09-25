5dff36c26118ca3977acf60ec56d4a947f7c6a28
yyss99 <yyssjj9999@gmail.com>
2023-09-25 11:33:10 +0900

    feat: ViewController에 테이블 뷰 구현

--- ProjectManager/ProjectManager/ViewController.swift
+++ ProjectManager/ProjectManager/ViewController.swift
@@ -7,16 +7,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var stackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
+        view.backgroundColor = .systemGray4
         configureNavigationBar()
+        configureTableViewStack()
     }
 
     func configureNavigationBar() {
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add
-                                                            , target: self, action: #selector(tapAddButton))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add,
+                                                            target: self,
+                                                            action: #selector(tapAddButton))
         navigationItem.title = "Project Manager"
         
         let navigationBarAppearance = UINavigationBarAppearance()
@@ -26,7 +30,49 @@ class ViewController: UIViewController {
     }
     
     @objc func tapAddButton() {
+
+    }
+    
+    func configureTableViewStack() {
+        stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        stackView.spacing = 10
         
+        view.addSubview(stackView)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        for _ in 0..<3 {
+            let tableView = UITableView()
+            tableView.delegate = self
+            tableView.dataSource = self
+            tableView.register(TableViewCell.self, forCellReuseIdentifier: "cell")
+            stackView.addArrangedSubview(tableView)
+        }
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        
+        cell.titleLabel.text = "Title TitleTitleTitleTitleTitleTitleTitleTitleTitleTitleTitleTitleTitleTitleTitleTitleTitleTitleTitleTitle\(indexPath.row + 1)"
+        cell.descriptionLabel.text = "Description \(indexPath.row + 1) DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription"
+        cell.dateLabel.text = "0000.00.00"
+        
+        return cell
     }
 }
 
